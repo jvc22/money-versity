67457b1b490447e22c8bb13406a918a2e0595700
joaovcostas <jvc22102003@gmail.com>
2024-04-08 17:29:19 -0300

    feat: create <header> layout

---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import { Home, Wallet } from 'lucide-react'
+import NavLink from './nav-link'
+import { Separator } from './ui/separator'
+import { ThemeToggle } from './theme/theme-toggle'
+
+export default function Header() {
+  return (
+    <header className="flex items-center gap-5">
+      <h1 className="text-lg font-bold">money.versity</h1>
+
+      <Separator orientation="vertical" className="h-6" />
+
+      <nav className="flex items-center gap-5">
+        <NavLink href="/">
+          <Home className="size-4" />
+          Dashboard
+        </NavLink>
+        <NavLink href="/transactions">
+          <Wallet className="size-4" />
+          Transactions
+        </NavLink>
+      </nav>
+
+      <div className="ml-auto">
+        <ThemeToggle />
+      </div>
+    </header>
+  )
+}
