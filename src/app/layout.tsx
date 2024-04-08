0016a197c477006b7f9b5cd0e65216cd2f12697c
joaovcostas <jvc22102003@gmail.com>
2024-04-08 17:30:42 -0300

    feat: add b<ody> classnames and add <header> to -layout-

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next'
+
 import { Inter as FontSans } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
+
+import Header from '@/components/header'
+import { ThemeProvider } from '@/components/theme/theme-provider'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -22,11 +26,19 @@ export default function RootLayout({
     <html lang="pt-br">
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          'min-h-screen bg-background font-sans antialiased max-w-6xl mx-auto p-5 flex flex-col gap-5',
           fontSans.variable,
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
