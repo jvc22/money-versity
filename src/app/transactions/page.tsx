8ef1038f44eebe2067ba339fee1bb5c51c2a8b7a
joaovcostas <jvc22102003@gmail.com>
2024-04-08 22:57:24 -0300

    update: map array to list <table-row>

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,5 +1,13 @@
 import { Button } from '@/components/ui/button'
 import { TransactionsFilters } from './filters'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { TransactionsTableRow } from './table-row'
 
 export default function Transactions() {
   return (
@@ -12,6 +20,26 @@ export default function Transactions() {
 
       <div className="space-y-2.5">
         <TransactionsFilters />
+
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[200px]">Amount</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead className="w-[200px]">Status</TableHead>
+                <TableHead className="w-[200px]">Category</TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 12 }).map((_, index) => (
+                <TransactionsTableRow key={index} />
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </div>
   )
