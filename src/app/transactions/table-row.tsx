c4c772b21cc3711d68f586371c2c7945304d0ace
joaovcostas <jvc22102003@gmail.com>
2024-04-08 22:56:59 -0300

    feat: create <table-row> component with transactions details

---
+++ src/app/transactions/table-row.tsx
@@ -0,0 +1,39 @@
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { TableCell, TableRow } from '@/components/ui/table'
+import { Search, X } from 'lucide-react'
+
+export function TransactionsTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant={'outline'} size={'xs'}>
+              <Search className="size-3" />
+              <span className="sr-only">Transaction details</span>
+            </Button>
+          </DialogTrigger>
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-medium">R$ 333,33</TableCell>
+      <TableCell>22th of October 2023</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="size-2 rounded-full bg-green-500" />
+          <span className="font-medium text-muted-foreground">Income</span>
+        </div>
+      </TableCell>
+      <TableCell>
+        <Badge variant={'secondary'}>Church</Badge>
+      </TableCell>
+      <TableCell>
+        <Button variant={'ghost'} size={'xs'}>
+          <X className="size-3 mr-2" />
+          Delete
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
