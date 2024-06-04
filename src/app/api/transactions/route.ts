a7bd60af4ff45eab5488bbb3cd5bf5a9c5200959
joaovcostas <jvc22102003@gmail.com>
2024-06-04 20:03:43 -0300

    fix: send categoryId to register relation on transaction table

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -4,36 +4,19 @@ import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
 
 export async function GET() {
-  const responseSchema = z.array(
-    z.object({
-      id: z.string(),
-      createdAt: z.string(),
-      amount: z.number(),
-      status: z.enum(['income', 'outcome']),
-      category: z.string(),
-      description: z.string(),
-    }),
-  )
-
   const transactions = await prisma.transactions.findMany()
 
-  const safeResponse = responseSchema.safeParse(transactions)
-  if (!safeResponse.success) {
-    console.error(safeResponse.error.format)
-    throw new Error()
-  }
-
-  return NextResponse.json(safeResponse.data, { status: 200 })
+  return NextResponse.json(transactions, { status: 200 })
 }
 
 export async function POST(request: Request) {
   const { formData } = await request.json()
 
   const bodySchema = z.object({
-    date: z.string(),
+    date: z.date(),
     amount: z.number(),
     status: z.enum(['income', 'outcome']),
-    category: z.string(),
+    categoryId: z.number(),
     description: z.string(),
   })
 
@@ -44,11 +27,11 @@ export async function POST(request: Request) {
   }
 
   const newTransaction = {
-    createdAt: formData.date,
-    amount: Math.round(formData.amount * 100) / 100,
-    status: formData.status,
-    category: formData.category,
-    description: formData.description,
+    createdAt: safeBody.data.date,
+    amount: Math.round(safeBody.data.amount * 100) / 100,
+    status: safeBody.data.status,
+    categoryId: safeBody.data.categoryId,
+    description: safeBody.data.description,
   }
 
   const transaction = await prisma.transactions.create({
