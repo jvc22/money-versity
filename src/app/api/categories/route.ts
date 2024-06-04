07886779f5a9f139a1926a7db4f02709e33188cc
joaovcostas <jvc22102003@gmail.com>
2024-06-04 20:11:31 -0300

    fix: remove unused zod parse

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,24 +1,13 @@
 import { NextResponse } from 'next/server'
-import { z } from 'zod'
 
 import { prisma } from '@/lib/prisma'
 
 export async function GET() {
-  const categoriesSchema = z.array(
-    z.object({
-      id: z.string(),
-      value: z.string(),
-      label: z.string(),
-    }),
-  )
-
   const categories = await prisma.categories.findMany()
 
-  const safeResponse = categoriesSchema.safeParse(categories)
-  if (!safeResponse.success) {
-    console.error(safeResponse.error.format)
-    return
+  if (categories) {
+    return NextResponse.json(categories, { status: 200 })
   }
 
-  return NextResponse.json(safeResponse.data, { status: 200 })
+  return NextResponse.json({}, { status: 404 })
 }
